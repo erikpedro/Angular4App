c5b84a55780d2f0fc4828bd12728c04579ab2b14
Erik Pedro <erikpedro@gmail.com>
2018-04-03 23:42:43 -0300

    sesao 6 aula 67

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -9,6 +9,8 @@ import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 8
+
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
     {label: 'Cartao de Debito', value: 'DEB'},
@@ -20,6 +22,9 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
   }
 
+itemsValue(): number {
+  return this.orderService.itemsValue()
+}
 
   cartItems(): CartItem[]{
     return this.orderService.cartItem()
--- meat-app-starter-master/src/app/order/order.service.ts
+++ meat-app-starter-master/src/app/order/order.service.ts
@@ -9,6 +9,10 @@ export class OrderService{
 
  constructor(private cartService: ShoppingCartService){}
 
+itemsValue(): number {
+  return this.cartService.total()
+}
+
 cartItem(): CartItem[]{
   return this.cartService.items
 }
