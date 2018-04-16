68184ccb52925d193a94429c8623d32cb2e9372a
Erik Pedro <erikpedro@gmail.com>
2018-04-15 22:39:07 -0300

    sesao 6 aula 68

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RadioOption} from '../shared/radio/radio-option.model'
 import {OrderService} from './order.service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
-
+import {Order, OrderItem} from "./order.model"
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
@@ -40,6 +40,16 @@ itemsValue(): number {
 
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+    .map((item: CartItem)=> new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe((orderId: string)=> {
+      console.log(`Compra concluida: ${orderId}`)
+      this.orderService.clear()
+    } )
+    console.log(order)
   }
 
 }
